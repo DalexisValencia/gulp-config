170e68688ff58880a44321ba96de50bad3689a72
DalexisValencia <d.alexis.valencia@gmail.com>
2021-03-31 14:23:13 -0500

    tareas para limpiar. mover y actualizar direcctorios

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,52 @@
 const { src, dest, gulp, watch, series } = require('gulp');
 const sass = require('gulp-sass');
-const glServer = require('gulp-live-server');
-const livereload = require('gulp-livereload');
+var connect = require('gulp-connect');
+var del = require('del');
+function moveFiles(){
+    return src('src/assets/**/*.*')
+    .pipe(dest('public/assets'));
+}
 function compileHtml(){
     return src('src/*.html')
-    .pipe(dest('public/'));
-    // .pipe(livereload());
+    .pipe(dest('public/'))
+    .pipe(connect.reload());
 }
 function compileScss() {
     return src('src/scss/*.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(dest('public/css/'));
-        // .pipe(livereload());
+        .pipe(dest('public/css/'))
+        .pipe(connect.reload());
 }
 
 exports.serve = function() {
-    glServer.static('public', 9000).start()
+    connect.server(
+        {
+            port: 8888,
+            livereload: true
+        }
+    );
 }
 
 function run() {
     exports.serve();
 }
+exports.deleteAll = function() {
+    return del([
+        'public/**',
+    ]);
+}
 
 function watcher() {
-    // livereload.listen();
     watch('src/scss/*.scss', {ignoreInitial: true}, compileScss);
     watch('src/*.html', compileHtml);
+    connect.reload();
 }
+
 exports.default = function(){
-    // livereload.listen();
+    exports.deleteAll();
     run();
     compileScss();
     compileHtml();
     watcher();
+    moveFiles();
 }
-
-

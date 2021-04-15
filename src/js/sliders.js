ecbe787202ef1de822970bd952e65a433d1d4e24
DalexisValencia <d.alexis.valencia@gmail.com>
2021-04-15 16:57:46 -0500

    Responsive para las flechas de navegación del banner

--- src/js/sliders.js
+++ src/js/sliders.js
@@ -1,9 +1,17 @@
-$( document ).ready(function() {
+$(document).ready(function() {
     $('.el-custom--banners-slides').slick({
         dots: true,
         appendDots: $('.el-custom--banners-dots'),
         arrows: false,
         autoplay: true,
+        responsive: [{
+            breakpoint: 900,
+            settings: {
+                dots: false,
+                arrows: true,
+                appendArrows: $('.el-custom--banners-dots'),
+            }
+        }]
     });
     $('#el-custom--flavors-card').slick({
         slidesToShow: 4,
@@ -11,36 +19,32 @@ $( document ).ready(function() {
         appendDots: $('.no-one'),
         arrows: false,
         autoplay: false,
-        responsive: [
-            {
-                breakpoint: 600,
-                settings: {
-                    slidesToShow: 2.35,
-                    slidesPerRow: 2.35,
-                    swipe: true,
-                    infinite: false,
-                }
+        responsive: [{
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2.35,
+                slidesPerRow: 2.35,
+                swipe: true,
+                infinite: false,
             }
-        ]
-   });
-   $('#el-custom--flavors__big-slide')                                                                                                                       .slick({
+        }]
+    });
+    $('#el-custom--flavors__big-slide').slick({
         slidesToShow: 3,
         dots: false,
         // appendDots: $('.no-one'),
         arrows: true,
         autoplay: false,
-        responsive: [
-            {
-                breakpoint: 600,
-                settings: {
-                    slidesToShow: 2.35,
-                    slidesPerRow: 2.35,
-                    swipe: true,
-                    infinite: false,
-                    arrows: false,
-                }
+        responsive: [{
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2.35,
+                slidesPerRow: 2.35,
+                swipe: true,
+                infinite: false,
+                arrows: false,
             }
-        ]
+        }]
     });
     //prueba:
     $('#el-custom--flavors-page__sliders').slick({
@@ -48,24 +52,22 @@ $( document ).ready(function() {
         appendDots: $('.el-custom--flavors-slider--navigation'),
         arrows: false,
         autoplay: false,
-        responsive: [
-            {
-                breakpoint: 900,
-                settings: {
-                    dots: false,
-                    swipe: true,
-                    infinite: false,
-                    arrows: true,
-                    appendArrows: $('.el-custom--flavors-slider--navigation'),
-                }
+        responsive: [{
+            breakpoint: 900,
+            settings: {
+                dots: false,
+                swipe: true,
+                infinite: false,
+                arrows: true,
+                appendArrows: $('.el-custom--flavors-slider--navigation'),
             }
-        ]
-    }).on('beforeChange', function(event, slick, currentSlide, nextSlide){
+        }]
+    }).on('beforeChange', function(event, slick, currentSlide, nextSlide) {
         console.log(nextSlide, 'pilas');
         $("header.el-custom--flavors-page__slider-navigation").attr('class', 'el-custom--flavors-page__slider-navigation');
-        $("header.el-custom--flavors-page__slider-navigation").addClass('header-'+nextSlide)
+        $("header.el-custom--flavors-page__slider-navigation").addClass('header-' + nextSlide)
 
-        $("#el-custom--flavors-page__sliders").removeClass().addClass('slick-initialized slick-slider bg-'+nextSlide)
-        $('.el-custom-flavors-page').removeClass().addClass('el-custom-flavors-page color-'+nextSlide);
-      });
+        $("#el-custom--flavors-page__sliders").removeClass().addClass('slick-initialized slick-slider bg-' + nextSlide)
+        $('.el-custom-flavors-page').removeClass().addClass('el-custom-flavors-page color-' + nextSlide);
+    });
 });

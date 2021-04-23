019e299bbc33bb1437212052f672a7bb690c344f
DalexisValencia <d.alexis.valencia@gmail.com>
2021-04-23 16:34:04 -0500

    Ajustes y creación de nuevo menú

--- src/js/burger.js
+++ src/js/burger.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
     $('.burger-menu').click(function() {
-        $('.el-custom--mobile-menu').show('slow');
+        $('.el-custom--main-menu').addClass('active');
     })
     $('.close-mobile-menu').click(function() {
-        $('.el-custom--mobile-menu').hide('slow');
+        $('.el-custom--main-menu').removeClass('active');
     })
 
     $(window).resize(function() {
         if ($(window).width() > 900) {
-            $('.el-custom--mobile-menu').hide('slow');
+            $('.el-custom--main-menu').removeClass('active')
         }
     })
 })

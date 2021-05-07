8e1889bbb7a487194998b9c86f53fe91f7f2b1b3
DalexisValencia <d.alexis.valencia@gmail.com>
2021-05-07 15:06:49 -0500

    Slide de precios en mobile

--- src/js/slidersProducts.js
+++ src/js/slidersProducts.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
     const bannerProducts = $(".el-custom--products-slide");
     if (bannerProducts.length) {
         bannerProducts.slick({
+            lazyLoad: 'ondemand',
             centerMode: true,
             slidesToShow: 5,
             dots: false,
@@ -11,36 +12,45 @@ $(document).ready(function() {
             autoplay: false,
             infinite: true,
             responsive: [
-                {
-                    breakpoint: 900,
-                    settings: {
-                        slidesToShow: 3,
-                        slidesPerRow: 3,
-                        adaptiveHeight: false,
-                        swipe: true,
-                        infinite: true,
-                        rows: 1,
-                    },
-                },
+                // {
+                //     breakpoint: 900,
+                //     settings: {
+                //         slidesToShow: 3,
+                //         slidesPerRow: 3,
+                //         adaptiveHeight: false,
+                //         swipe: true,
+                //         infinite: true,
+                //         rows: 1,
+                //     },
+                // },
                 {
                     breakpoint: 600,
                     settings: {
+                        speed: 700,
                         centerMode: true,
-                        slidesToShow: 2,
-                        slidesPerRow: 2,
-                        adaptiveHeight: true,
-                        swipe: true,
-                        infinite: true,
                         rows: 1,
-                    },
+                        slidesToShow: 1.72,
+                        infinite: true
+                    }
+                    // settings: {
+                    //     centerMode: true,
+                    //     slidesToShow: 1,
+                    //     dots: false,
+                    //     arrows: true,
+                    //     adaptiveHeight: true,
+                    //     appendArrows: $(".el-custom--products-arrows"),
+                    //     autoplay: false,
+                    //     infinite: true,
+                    // },
                 },
             ]
-        }).on('beforeChange', function(event, slick, currentSlide, nextSlide) {
-            console.log(nextSlide, 'pilas');
-        });
-        $("div.products--card").click(function(){
-            const index = $(this).attr('data-attr-index');
-            bannerProducts.slick('slickGoTo', index -1);
-        });
+        })
+        // .on('beforeChange', function(event, slick, currentSlide, nextSlide) {
+        //     console.log(nextSlide, 'pilas');
+        // });
+        // $("div.products--card").click(function(){
+        //     const index = $(this).attr('data-attr-index');
+        //     bannerProducts.slick('slickGoTo', index -1);
+        // });
     }
 });

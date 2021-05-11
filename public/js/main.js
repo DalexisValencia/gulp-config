01b074494a635597aecf6008b5b130672bc8dfe4
DalexisValencia <d.alexis.valencia@gmail.com>
2021-05-11 15:56:13 -0500

    Responsive y ajustes del modal

--- public/js/main.js
+++ public/js/main.js
@@ -12,6 +12,28 @@ $(document).ready(function() {
         }
     })
 })
+
+var modal = document.getElementById("modalRegister");
+
+var btn = document.getElementById("openModal");
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 // $(document).ready(function() {
 //     $('select').niceSelect();
 // });

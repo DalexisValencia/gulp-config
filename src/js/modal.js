0ac8fb1888a6df71621ca51d576ba34998ad515b
DalexisValencia <d.alexis.valencia@gmail.com>
2021-05-12 08:12:44 -0500

    Ajustes

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,25 +1,19 @@
 
-var modal = document.getElementById("modalRegister");
+var modal = $("#modalRegister");
 
-var btn = document.getElementById("openModal");
-var span = document.getElementsByClassName("close")[0];
+var btn = $("#openModal");
+var span = $(".close");
 
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
+btn.click(function() {
+    modal.css("display", "block");
+    $("body").addClass("no-overflow");
+    window.scrollTo(0, 0);
+});
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+span.click(function() {
+    modal.css("display", "none");
+    $("body").removeClass("no-overflow");
+});
 
 // (function ($, Drupal) {
 

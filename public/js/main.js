a4a1de4ab8eaeb9f5ad3c2d71c348a0efe96f2cf
DalexisValencia <d.alexis.valencia@gmail.com>
2021-05-20 17:42:07 -0500

    Ajustes de slider, menu mobile, menu, fuentes, colores y responsive

--- public/js/main.js
+++ public/js/main.js
@@ -232,16 +232,17 @@ $(document).ready(function() {
     const bannerProducts = $(".el-custom--products-slide");
     if (bannerProducts.length) {
         bannerProducts.slick({
+            focusOnSelect: true,
             lazyLoad: 'ondemand',
             centerMode: true,
-            slidesToShow: 5,
+            slidesToShow: 4.99,
             dots: false,
             arrows: true,
             adaptiveHeight: true,
             appendArrows: $(".el-custom--products-arrows"),
             autoplay: false,
             infinite: true,
-            speed: 700,
+            speed: 400,
             responsive: [
                 // {
                 //     breakpoint: 900,
@@ -254,23 +255,23 @@ $(document).ready(function() {
                 //         rows: 1,
                 //     },
                 // },
-                {
-                    breakpoint: 900,
-                    settings: {
-                        centerMode: true,
-                        rows: 1,
-                        slidesToShow: 4,
-                        infinite: true,
-                        variableWidth: true
-                    }
-                },
-                {
-                    breakpoint: 700,
-                    settings: {
-                        slidesToShow: 3,
-                        variableWidth: true
-                    }
-                },
+                // {
+                //     breakpoint: 740,
+                //     settings: {
+                //         centerMode: true,
+                //         rows: 1,
+                //         slidesToShow: 4,
+                //         infinite: true,
+                //         variableWidth: true
+                //     }
+                // },
+                // {
+                //     breakpoint: 700,
+                //     settings: {
+                //         slidesToShow: 3,
+                //         variableWidth: true
+                //     }
+                // },
                 {
                     breakpoint: 600,
                     settings: {
@@ -291,12 +292,35 @@ $(document).ready(function() {
                 },
             ]
         })
-        // .on('beforeChange', function(event, slick, currentSlide, nextSlide) {
-        //     console.log(nextSlide, 'pilas');
-        // });
+        .on('afterChange', function(event, slick, currentSlide, nextSlide) {
+            // console.log(nextSlide, 'pilas');
+            hidethirdInactive();
+        })
+        .on('init', function(event, slick, currentSlide, nextSlide) {
+            // console.log(nextSlide, 'pilas');
+            hidethirdInactive();
+        });
         $("div.products--card").click(function(){
             const index = $(this).attr('data-attr-index');
             bannerProducts.slick('slickGoTo', index -1);
+            hidethirdInactive();
         });
+
+        function hidethirdInactive() {
+            const realWidth = jQuery( window ).width();
+            $("div.products--card").removeClass('no-visible');
+            if(realWidth > 600) {
+                $("div.products--card.slick-slide.slick-current.slick-active.slick-center").prev().prev().prev().addClass('no-visible');
+                $("div.products--card.slick-slide.slick-current.slick-active.slick-center").next().next().next().addClass('no-visible');
+            }
+            else if (realWidth < 600) {
+                $("div.products--card.slick-slide.slick-current.slick-active.slick-center").prev().addClass('no-visible');
+                $("div.products--card.slick-slide.slick-current.slick-active.slick-center").next().addClass('no-visible');
+            }
+        }
     }
+
+    $(window).resize(function(){
+        hidethirdInactive();
+    });
 });

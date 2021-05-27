09e0788ca2424811abe2536afc51f7be4de2c3ba
DalexisValencia <d.alexis.valencia@gmail.com>
2021-05-27 13:22:04 -0500

    banners

--- public/js/main.js
+++ public/js/main.js
@@ -68,128 +68,6 @@ $(document).ready(function() {
     });
     }
 });
-
-// (function ($, Drupal) {
-
-//     //Se agrega el checkmark desde js
-//     $("label[for='edit-tyc']").prepend('<span class="checkmark"></span>');
-//     $("label[for='edit-marketing']").prepend('<span class="checkmark"></span>');
- 
-//    $('form').validate({
-//      ignore: [],
-//      rules: {
-//        "first_name": {
-//          required: true,
-//          spanish_words: true,
-//        },
-//        "last_name": {
-//          required: true,
-//          spanish_words: true,
-//        },
-//        "email": {
-//          required: true,
-//          email: true,
-//          emailname: true,
-//          email_exists: true,
-//          emailDomainRestriction: true,
-//        },
-//        "phone": {
-//          required: true,
-//          phone_exists: true,
-//          maxlength: 9,
-//          //rangelength: [9,9],
-          
-//        },
-//        "gender": {
-//          required: true,
-//          //validatemyselect: true,
-//        },
-//        "month": {
-//          required: true,
-//        },
-//        "day": {
-//          required: true,
-//        },
-//        "year": {
-//          required: true,
-//          agecheck: true,
-//          validdate: true,
-//        },
-//        "city": {
-//          required: true,
-//        },
-//        "DNI": {
-//          required: true,
-//          dni_exists: true,
-//          //maxlength: 11, 
-//          //minlength: 8,
-//          rangelength: [8,11],
-//        },
-//        "tyc": {
-//          required: true,
-//        }
-//      },
- 
-//      messages: {
-//          "first_name": {
-//            required: "Nombre inválido, debes ingresar un nombre válido",
-//          },
-//          "last_name": {
-//            required: "Apellido inválido, debes ingresar un apellido válido",
-//          },
-//          "email": {
-//            required: "Dirección de correo electrónico no válida",
-//            email: "Dirección de correo electrónico no válida",
-//            email_exists: "La dirección de correo que ingresaste ya fue registrada",
-//          },
-//          "phone": {
-//            required: "Debes ingresar tu número de teléfono",
-//            maxlength: "Maximo 9 Caracteres ",
-//            //rangelength: "Debe ingresar 9 Caracteres",
-//          },
-//          "gender": {
-//            required: "Debes seleccionar tu género",
-//          },
-//          "month": {
-//            required: "Ingresar mes de nac",
-//          },
-//          "day": {
-//            required: "Ingresar día de nac",
-//          },
-//          "year": {
-//            required: "Ingresar año de nac",
-//          },   
-//          "DNI": {
-//            required: "Debes ingresar un DNI/carnet extranjería válido",
-//            maxlength: "Maximo 11 Caracteres",
-//            minlength: "Minimo 8 Caracteres",
-//            rangelength: "Debe ingresar Un nuemero entre 8 y 11 caracteres",
-//          },
-//          "city": {
-//            required: "Debes seleccionar ciudad de residencia",
-//          },
-//          "tyc": {
-//            required: "Debes aceptar términos y condiciones para continuar",
-//          },
-//          "question1": {
-//            required: "Debes seleccionar una respuesta",
-//          },
-//          "question2": {
-//            required: "Debes seleccionar una respuesta",
-//          }
-//        },
- 
-//       /* errorPlacement: function(error, element) {
-//          if (element.is('gender:hidden')) {
-//              error.insertAfter(element.next('.nice-select'));
-//          } else {
-//              error.insertAfter(element);
-//          }
-//      }*/
-              
-//    });
- 
-//  }) (jQuery, Drupal);
 $(document).ready(function() {
     $('select').niceSelect();
 });
@@ -210,59 +88,6 @@ $(document).ready(function() {
             }
         }]
     });
-    // $('#el-custom--flavors__big-slide').slick({
-    //     slidesToShow: 3,
-    //     dots: false,
-    //     // appendDots: $('.no-one'),
-    //     arrows: true,
-    //     autoplay: false,
-    //     responsive: [{
-    //         breakpoint: 600,
-    //         settings: {
-    //             slidesToShow: 2.35,
-    //             slidesPerRow: 2.35,
-    //             swipe: true,
-    //             infinite: false,
-    //             arrows: false,
-    //         }
-    //     }]
-    // });
-    // //prueba:
-    
-    // $('#el-custom--flavors-page__sliders').slick({
-    //     dots: true,
-    //     appendDots: $('.el-custom--flavors-slider--navigation'),
-    //     arrows: false,
-    //     autoplay: false,
-    //     responsive: [{
-    //         breakpoint: 900,
-    //         settings: {
-    //             dots: false,
-    //             swipe: true,
-    //             infinite: true,
-    //             arrows: true,
-    //             appendArrows: $('.el-custom--flavors-slider--navigation'),
-    //         }
-    //     }]
-    // }).on('beforeChange', function(event, slick, currentSlide, nextSlide) {
-    //     console.log(nextSlide, 'pilas');
-    //     $("header.el-custom--flavors-page__slider-navigation").attr('class', 'el-custom--flavors-page__slider-navigation');
-    //     $("header.el-custom--flavors-page__slider-navigation").addClass('header-' + nextSlide)
-
-    //     $("#el-custom--flavors-page__sliders").removeClass().addClass('slick-initialized slick-slider bg-' + nextSlide)
-    //     $('.el-custom-flavors-page').removeClass().addClass('el-custom-flavors-page color-' + nextSlide);
-    // });
-
-    // var pathname = window.location.pathname;
-
-    // if(pathname.indexOf('sabores') != -1) {
-    //     var findIndex = pathname.split("/")[2];
-    //     console.error(findIndex, 'indes');
-    //     if(findIndex != null){
-    //         console.info('ahora vamos')
-    //         // $('#el-custom--flavors-page__sliders').slick('slickGoTo', 3);
-    //     }
-    // }
 });
 $(document).ready(function() {
     const bannerProducts = $(".el-custom--products-slide");
@@ -383,8 +208,6 @@ $(document).ready(function() {
 });
 $(document).ready(function(){
     const logo = $('img.logo');
-    console.error(logo)
-    // console.error(logo.hasClass( ".cristal" ), 'logo.hasClass( ".cristal" )')
     if(logo.hasClass( "cristal" )) {
         logo.attr("src", "assets/logo-cristal.png");
     }
--- src/js/theme.js
+++ src/js/theme.js
@@ -1,7 +1,5 @@
 $(document).ready(function(){
     const logo = $('img.logo');
-    console.error(logo)
-    // console.error(logo.hasClass( ".cristal" ), 'logo.hasClass( ".cristal" )')
     if(logo.hasClass( "cristal" )) {
         logo.attr("src", "assets/logo-cristal.png");
     }

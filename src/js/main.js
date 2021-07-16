575aec8d85459c72f0bac61c800827632b98c44b
DalexisValencia <d.alexis.valencia@gmail.com>
2021-07-16 18:04:20 -0500

    removiendo basura

--- src/js/main.js
+++ src/js/main.js
@@ -12,7 +12,7 @@
 $(document).ready(function() {
     var slidersGenreation = $(".abi--clubcolombia-flavor-travel__products.owl-carousel");
     if (slidersGenreation.length >= 1) {
-        var isiPad = navigator.userAgent.toLowerCase().indexOf("ipad");
+        // var isiPad = navigator.userAgent.toLowerCase().indexOf("ipad");
 
         slidersGenreation.owlCarousel({
             items: 4,
@@ -36,49 +36,6 @@ $(document).ready(function() {
                     items: 4
                 }
             }
-        });
-
-        $(".play-gallery-video").on("click", function(event) {
-            const posX = event.pageX;
-            const videoWrapper = $(".abi--doblemalta-wrapper-generationThatBroke--video");
-            const url = $(this).attr("attr-video");
-            if (url) {
-                videoWrapper.css({ "left": posX + "px", "opacity": "1" });
-                videoWrapper.addClass("start-animation");
-                setTimeout(() => {
-                    videoWrapper.removeClass("start-animation").addClass("show-video-animation");
-                    setTimeout(() => {
-                        $('<iframe>') // Creates the element
-                            .attr('src', url) // Sets the attribute spry:region="myDs"
-                            .attr('allow', 'autoplay')
-                            .attr('class', 'generated-iframe')
-                            .appendTo('.abi--doblemalta-wrapper-generationThatBroke--video');
-                    }, 150);
-                }, 400);
-            }
-        })
-
-        $(".close-video").on("click", function() {
-            const videoWrapper = $(".abi--doblemalta-wrapper-generationThatBroke--video");
-            $('.generated-iframe').remove();
-            videoWrapper.css('opacity', '0');
-            videoWrapper.attr('style', '');
-            videoWrapper.removeClass('start-animation').removeClass('show-video-animation');
-        });
-    }
-
-    var mainBanners = $(".abi--doblemalta__container-slide.owl-carousel");
-
-    if (mainBanners.length == 1) {
-        mainBanners.owlCarousel({
-            loop: true,
-            autoplay: true,
-            items: 1,
-            nav: false,
-            autoplayHoverPause: true,
-            animateOut: 'slideOutUp',
-            animateIn: 'slideInUp',
-            dotsContainer: $('.abi--doblemalta__container-slide-dots')
         });
     }
 })

910941e086f41cf1304f2ddba8d788ab7ffa3948
DalexisValencia <d.alexis.valencia@gmail.com>
2021-07-19 17:38:40 -0500

    avance del proyecto

--- public/js/main.js
+++ public/js/main.js
@@ -10,32 +10,60 @@
 // })(jQuery);
 
 $(document).ready(function() {
-    var slidersGenreation = $(".abi--clubcolombia-flavor-travel__products.owl-carousel");
+    var slidersGenreation = $(".abi--main-sliders.owl-carousel");
     if (slidersGenreation.length >= 1) {
         // var isiPad = navigator.userAgent.toLowerCase().indexOf("ipad");
 
         slidersGenreation.owlCarousel({
-            items: 4,
+            items: 1,
             loop: true,
-            responsive: {
-                0: {
-                    items: 1,
-                    marging: 10,
-                    center: true,
-                    navContainer: $(".abi--clubcolombia-flavor-travel__carousel-nav")
-                },
-                590: {
-                    items: 2,
-                    navContainer: $(".abi--clubcolombia-flavor-travel__carousel-nav")
-                },
-                760: {
-                    items: 3,
-                    navContainer: $(".abi--clubcolombia-flavor-travel__carousel-nav")
-                },
-                960: {
-                    items: 4
-                }
-            }
+            navContainer: $(".abi--main-sliders-navigation"),
+            dotsContainer: $(".abi--main-sliders-dots")
+                // responsive: {
+                //     0: {
+                //         items: 1,
+                //         marging: 10,
+                //         center: true,
+                //     },
+                //     590: {
+                //         items: 2,
+                //         navContainer: $(".abi--clubcolombia-flavor-travel__carousel-nav")
+                //     },
+                //     760: {
+                //         items: 3,
+                //         navContainer: $(".abi--clubcolombia-flavor-travel__carousel-nav")
+                //     },
+                //     960: {
+                //         items: 4
+                //     }
+                // }
         });
+
+        var slidersHome = $(".abi--main-container--body-sliders.owl-carousel");
+        if (slidersHome.length >= 1) {
+            slidersHome.owlCarousel({
+                items: 4,
+                loop: true,
+                responsive: {
+                    0: {
+                        items: 1,
+                        marging: 10,
+                        center: true,
+                        navContainer: $(".abi--main-container--body-sliders--navigation")
+                    },
+                    590: {
+                        items: 2,
+                        navContainer: $(".abi--main-container--body-sliders--navigation")
+                    },
+                    760: {
+                        items: 3,
+                        navContainer: $(".abi--main-container--body-sliders--navigation")
+                    },
+                    960: {
+                        items: 4
+                    }
+                }
+            });
+        }
     }
 })

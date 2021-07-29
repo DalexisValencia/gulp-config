89aaf194b91908fa705e4b3cb091b833afc96fe4
DalexisValencia <d.alexis.valencia@gmail.com>
2021-07-29 14:21:37 -0500

    social section

--- src/js/main.js
+++ src/js/main.js
@@ -18,6 +18,45 @@ $(document).ready(function() {
         drawer.removeClass('active');
     });
 
+    var sliderCarouselNewPilsenExtra = $(".abi--center-banner-social--gallery-wrapper-sliders.owl-carousel")
+
+    if (sliderCarouselNewPilsenExtra.length >= 1) {
+
+        sliderCarouselNewPilsenExtra.owlCarousel({
+            items: 4,
+            loop: true,
+            responsive: {
+                0: {
+                    items: 1.5,
+                    margin: 10,
+                    center: false,
+                    navContainer: $(".abi--main-container--gallery-wrapper-sliders--navigation")
+                },
+                420: {
+                    margin: 10,
+                    items: 2.8,
+                    navContainer: $(".abi--main-container--gallery-wrapper-sliders--navigation")
+                },
+                600: {
+                    margin: 10,
+                    items: 3.5,
+                    navContainer: $(".abi--main-container--gallery-wrapper-sliders--navigation")
+                },
+                710: {
+                    margin: 10,
+                    items: 4,
+                    navContainer: $(".abi--main-container--gallery-wrapper-sliders--navigation")
+                },
+                960: {
+                    items: 4,
+                    loop: false,
+                    navContainer: $(".abi--main-container--gallery-wrapper-sliders--navigation")
+                }
+            }
+        });
+
+    }
+
     var slidersGenreation = $(".abi--main-sliders.owl-carousel");
     if (slidersGenreation.length >= 1) {
         // var isiPad = navigator.userAgent.toLowerCase().indexOf("ipad");
@@ -46,6 +85,7 @@ $(document).ready(function() {
                 //     }
                 // }
         });
+
 
         var slidersHome = $(".abi--main-container--body-sliders.owl-carousel");
         if (slidersHome.length >= 1) {

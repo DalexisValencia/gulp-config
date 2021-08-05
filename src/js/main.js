b2e52a7c8db0e92e7b2af4d86a93036b290419ba
DalexisValencia <d.alexis.valencia@gmail.com>
2021-08-05 08:43:03 -0500

    limpiando proyecto

--- src/js/main.js
+++ src/js/main.js
@@ -17,6 +17,13 @@ $(document).ready(function() {
                 iconChild.removeClass(closeClassName).addClass("icon-burger-menu");
             }
         });
+
+        $(".abi--main-bannervideo--play-btn").on("click", function() {
+            var iframe = $(".abi--main-bannervideo--video").find("iframe");
+            var autoplay = iframe.attr("src") + "?autoplay=true";
+            iframe.attr("src", autoplay);
+            $(".abi--main-bannervideo--video").addClass("active")
+        });
         // $(".close-drawer").on("click", function() {
         //     drawer.removeClass('active');
         // });
@@ -25,101 +32,15 @@ $(document).ready(function() {
         const openForm = $(".register-now");
         const closeForm = $(".abi--overlay-register--form-close");
 
-        // openForm.click(function() {
-        //     $(".abi--overlay-register").addClass("active");
-        //     $("body").addClass("no-scroll");
-        // });
+        openForm.click(function() {
+            $(".abi--overlay-register").addClass("active");
+            $("body").addClass("no-scroll");
+        });
 
         closeForm.click(function() {
             $(".abi--overlay-register").removeClass("active");
             $("body").removeClass("no-scroll");
         });
 
-        var sliderCarouselNewPilsenExtra = $(".abi--center-banner-social--gallery-wrapper-sliders.owl-carousel")
-        if (sliderCarouselNewPilsenExtra.length >= 1) {
-
-            sliderCarouselNewPilsenExtra.owlCarousel({
-                items: 4,
-                loop: true,
-                responsive: {
-                    0: {
-                        items: 1.5,
-                        margin: 10,
-                        center: false,
-                        navContainer: $(".abi--main-container--gallery-wrapper-sliders--navigation")
-                    },
-                    420: {
-                        margin: 10,
-                        items: 2.8,
-                        navContainer: $(".abi--main-container--gallery-wrapper-sliders--navigation")
-                    },
-                    600: {
-                        margin: 10,
-                        items: 3.5,
-                        navContainer: $(".abi--main-container--gallery-wrapper-sliders--navigation")
-                    },
-                    710: {
-                        margin: 10,
-                        items: 4,
-                        navContainer: $(".abi--main-container--gallery-wrapper-sliders--navigation")
-                    },
-                    960: {
-                        items: 4,
-                        loop: false,
-                        navContainer: $(".abi--main-container--gallery-wrapper-sliders--navigation")
-                    }
-                }
-            });
-
-        }
-        // Sliders main Home
-        var slidersGenreation = $(".abi--main-sliders.owl-carousel");
-        if (slidersGenreation.length >= 1) {
-            // var isiPad = navigator.userAgent.toLowerCase().indexOf("ipad");
-
-            slidersGenreation.owlCarousel({
-                items: 1,
-                loop: true,
-                navContainer: $(".abi--main-sliders-navigation"),
-                dotsContainer: $(".abi--main-sliders-dots")
-            });
-
-            // Sliders social network on home
-            var slidersHome = $(".abi--main-container--body-sliders.owl-carousel");
-            if (slidersHome.length >= 1) {
-                slidersHome.owlCarousel({
-                    items: 4,
-                    loop: true,
-                    responsive: {
-                        0: {
-                            items: 1.5,
-                            margin: 10,
-                            center: false,
-                            navContainer: $(".abi--main-container--body-sliders--navigation")
-                        },
-                        420: {
-                            margin: 10,
-                            items: 2.8,
-                            navContainer: $(".abi--main-container--body-sliders--navigation")
-                        },
-                        600: {
-                            margin: 10,
-                            items: 3.5,
-                            navContainer: $(".abi--main-container--body-sliders--navigation")
-                        },
-                        710: {
-                            margin: 10,
-                            items: 4,
-                            navContainer: $(".abi--main-container--body-sliders--navigation")
-                        },
-                        960: {
-                            items: 4,
-                            loop: false,
-                            navContainer: $(".abi--main-container--body-sliders--navigation")
-                        }
-                    }
-                });
-            }
-        }
     })
     // })(jQuery);

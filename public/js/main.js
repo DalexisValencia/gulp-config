a1ca5238feef6b003bede1c3e4b371635353c28e
DalexisValencia <d.alexis.valencia@gmail.com>
2021-09-09 16:42:38 -0500

    removiendo codigo de los sliders

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,6 @@
 // jQuery(document).ready(function() {
 $(document).ready(function() {
-        //addinf lateral teselation only new pilse extra page
-        var areIncorrectPage = $(".abi--top-banner-pilsenextra").length
-        console.info(areIncorrectPage, 'encontrada')
-        if (areIncorrectPage && ($("body.agegate-visible").length == 0)) {
-            $("footer.abi--main-main-footer").addClass("lateral-tessellation");
-        }
+        //open an close drawer
         var drawer = $(".abi--main-navigation");
         $(".open-drawer").on("click", function() {
             $('body').addClass('no-scroll');
@@ -15,58 +10,6 @@ $(document).ready(function() {
             $('body').removeClass('no-scroll');
             drawer.removeClass('active');
         });
-        // Sliders social network on new pilsen extra
-        var sliderCarouselNewPilsenExtra = $(".abi--center-banner-social--gallery-wrapper-sliders.owl-carousel")
-        if (sliderCarouselNewPilsenExtra.length >= 1) {
-
-            sliderCarouselNewPilsenExtra.owlCarousel({
-                items: 4,
-                loop: true,
-                responsive: {
-                    0: {
-                        items: 1.5,
-                        margin: 10,
-                        center: false,
-                        navContainer: $(".abi--main-container--gallery-wrapper-sliders--navigation")
-                    },
-                    420: {
-                        margin: 10,
-                        items: 2.8,
-                        navContainer: $(".abi--main-container--gallery-wrapper-sliders--navigation")
-                    },
-                    600: {
-                        margin: 10,
-                        items: 3.5,
-                        navContainer: $(".abi--main-container--gallery-wrapper-sliders--navigation")
-                    },
-                    710: {
-                        margin: 10,
-                        items: 4,
-                        navContainer: $(".abi--main-container--gallery-wrapper-sliders--navigation")
-                    },
-                    960: {
-                        items: 4,
-                        loop: false,
-                        navContainer: $(".abi--main-container--gallery-wrapper-sliders--navigation")
-                    }
-                }
-            });
-
-        }
-        // Sliders main Home
-        var slidersGenreation = $(".abi--main-sliders.owl-carousel");
-        if (slidersGenreation.length >= 1) {
-            // var isiPad = navigator.userAgent.toLowerCase().indexOf("ipad");
-
-            slidersGenreation.owlCarousel({
-                items: 1,
-                loop: true,
-                navContainer: $(".abi--main-sliders-navigation"),
-                dotsContainer: $(".abi--main-sliders-dots")
-            });
-
-            // Sliders social network on home
-        }
         //social slider
         var slidersHome = $(".abi--main-container--body-sliders.owl-carousel");
         if (slidersHome.length >= 1) {

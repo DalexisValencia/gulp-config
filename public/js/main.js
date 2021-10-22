6ffde1943c27a9404c9de415b55f91a0d315fb02
DalexisValencia <d.alexis.valencia@gmail.com>
2021-10-22 16:49:27 -0500

    Ajustes

--- public/js/main.js
+++ public/js/main.js
@@ -16,9 +16,11 @@ $(document).ready(function() {
         var sliderHomeDotsNavigation = $(".abi-main--home--section-gallery--singers__navigation--dots")
         if (slidersHome.length >= 1 && sliderHomeNavigation.length >= 1) {
             slidersHome.owlCarousel({
-                items: 4,
+                items: 3,
                 loop: true,
                 video: true,
+                touchDrag: false,
+                mouseDrag: false,
                 responsive: {
                     // 0: {
                     //     items: 1,
@@ -30,14 +32,14 @@ $(document).ready(function() {
                     // },
                     0: {
                         margin: 10,
-                        items: 2.5,
+                        items: 3,
                         nav: true,
                         navContainer: sliderHomeNavigation,
                         dotsContainer: sliderHomeDotsNavigation,
                     },
                     600: {
                         margin: 10,
-                        items: 2.5,
+                        items: 3,
                         nav: true,
                         navContainer: sliderHomeNavigation,
                         dotsContainer: sliderHomeDotsNavigation,
@@ -51,7 +53,7 @@ $(document).ready(function() {
                     },
                     960: {
                         items: 3,
-                        margin: 50,
+                        margin: 10,
                         loop: true,
                         nav: true,
                         navContainer: sliderHomeNavigation,

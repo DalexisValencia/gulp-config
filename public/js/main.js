74060db1d35dadf127ee2288f561a76088fd351f
DalexisValencia <d.alexis.valencia@gmail.com>
2021-10-25 12:07:56 -0500

    ajustes

--- public/js/main.js
+++ public/js/main.js
@@ -6,28 +6,15 @@ $(document).ready(function() {
             jQuery(window).on("scroll", function() {
                 var header = jQuery("header.header");
                 var currentScroll = jQuery(window).scrollTop();
-                // console.info(currentScroll, 'currentScroll');
 
-                // console.error(currentScroll > 0 && !header.hasClass("hiddeScroll"))
-
-                if (currentScroll > 0 && !header.hasClass("hiddeScroll")) {
-                    // console.log("dentro")
+                if (currentScroll > lastScroll) {
                     header.addClass("hiddeScroll");
-                    return;
-                }
-
-                if (currentScroll <= 0 && header.hasClass("hiddeScroll")) {
+                    console.info("downscroll code")
+                } else {
                     header.removeClass("hiddeScroll");
-                    return;
+                    console.info("upscroll code")
                 }
-
-                // if (currentScroll > lastScroll && header.hasClass("hiddeScroll")) {
-                //     header.removeClass("hiddeScroll");
-                // }
-
                 lastScroll = currentScroll;
-                console.info(currentScroll, 'currentScroll')
-                console.info(lastScroll, 'lastScroll')
             });
         }
         var slidersGenreation = $(".abi--clubcolombia-flavor-travel__products.owl-carousel");
